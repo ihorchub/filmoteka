a4bf5172992ed911df1bf1f166c21438713b0595
Serge-Moskalenko <skmos2019@outlook.com>
2022-11-06 13:25:48 +0000

    test

---
+++ src/js/clickOnMovie.js
@@ -0,0 +1,11 @@
+export async function clickOnMovie(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG' && e.target.nodeName !== 'H2') {
+    return;
+    }
+  
+    apiServise.movieId = e.path[2].id
+
+    apiServise.fetchId().then(console.log);
+};
