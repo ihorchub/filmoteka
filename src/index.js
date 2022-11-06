09be231a15d3e2fbeb94b3e7cc33744fb906d822
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-11-06 16:13:16 +0200

    trailer on

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import  ApiServise  from "./js/API";
-import { onSubmit } from "./js/onSubmit";
-import { renderCards } from "./js/renderCards";
-import { clickOnMovie } from "./js/clickOnMovie";
+import ApiServise from './js/API';
+import { onSubmit } from './js/onSubmit';
+import { renderCards } from './js/renderCards';
+// import { clickOnMovie } from './js/clickOnMovie';
+import trailer from './js/film-trailer.js';
 
 export const refs = {
-    searchForm: document.querySelector('.home-header__form'),
-    cardHolder: document.querySelector('.card-holder'),
-    conteiner: document.querySelector('.card-container.container')
-}
+  searchForm: document.querySelector('.home-header__form'),
+  cardHolder: document.querySelector('.card-holder'),
+  conteiner: document.querySelector('.card-container.container'),
+};
 
 const apiServise = new ApiServise();
 
-refs.searchForm.addEventListener("submit", onSubmit);
-refs.conteiner.addEventListener('click',clickOnMovie)
+refs.searchForm.addEventListener('submit', onSubmit);
+refs.cardHolder.addEventListener('click', onCardClick);
+// refs.conteiner.addEventListener('click', clickOnMovie);
 
-apiServise.fetchDefault().then(data => { renderCards(data);});
+apiServise.fetchDefault().then(data => {
+  renderCards(data);
+});
+
+function onCardClick(e) {
+  if (e.target === e.currentTarget) return;
+
+  trailer.showTrailer(e.target.closest('li').id);
+}
