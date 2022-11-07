b4436ce6d03d32805589eddf33d2e6bd1ad52bf6
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-07 14:16:16 +0200

    Adds local-storage workflow
    - Button "Add to Watched" in movie modal
    - Button "Add to Queue" in movie modal

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -2,7 +2,7 @@
 let item = '';
 
 // 1. reach the btn
-const addQueueRef = document.querySelector('.js-movie-modal__btn');
+// const addQueueRef = document.querySelector('.js-movie-modal__btn');
 
 // 2. create variable for localstorage's value
 const moviesQueue = JSON.parse(localStorage.getItem('movies-queue')) || [];
