19a2b8000e499fe8a1c5401f117b4f348bad0dad
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-08 16:23:34 +0200

    Adds comments to local-storage feature
    And removes local-storage.js (for now the related code is in film-modal.js file)

--- src/js/film-modal.js
+++ src/js/film-modal.js
@@ -3,8 +3,9 @@ const filmModalMask = document.querySelector('.js-movie-modal-mask');
 
 filmModalMask.addEventListener('click', closeModal);
 
-const moviesQueue = [];
+// variables declaration for further local storage content
 const moviesWatched = [];
+const moviesQueue = [];
 
 export function showModal(data) {
   renderModal(data);
@@ -14,22 +15,27 @@ export function showModal(data) {
   filmModalMask.classList.remove('is-hidden');
   window.addEventListener('keydown', onEscKeyPress);
 
-  const addQueueRef = document.querySelector('.add-queue');
+  // reaching "Add to Watched" and "Add to Queue" buttons
   const addWatched = document.querySelector('.add-watched');
+  const addQueueRef = document.querySelector('.add-queue');
 
-  addQueueRef.addEventListener('click', onQueueClick);
-  function onQueueClick() {
-    if (!moviesQueue.find(item => item.id === data.id)) {
-      moviesQueue.push(data);
-      localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
-    }
-  }
-
+  // adding listeners to "Add to Watched" and "Add to Queue" buttons by clicking to "Add to Watched"
   addWatched.addEventListener('click', onWatchedClick);
+  addQueueRef.addEventListener('click', onQueueClick);
+
+  // functioin of adding to "Watched" to the local storage
   function onWatchedClick() {
     if (!moviesWatched.find(item => item.id === data.id)) {
       moviesWatched.push(data);
       localStorage.setItem('movies-watched', JSON.stringify(moviesWatched));
+    }
+  }
+
+  // functioin of adding to "Queue" to the local storage by clicking "Add to Queue"
+  function onQueueClick() {
+    if (!moviesQueue.find(item => item.id === data.id)) {
+      moviesQueue.push(data);
+      localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
     }
   }
 }
