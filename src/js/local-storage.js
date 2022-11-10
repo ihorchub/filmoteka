061f34021d212953b1ea3187a332d82f5dfa7a86
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-10 10:58:34 +0200

    Adds function for add/remove from local storage by click

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -20,21 +20,25 @@ export function locStorage(data) {
     }
   }
 
-  // functioin of adding to "Watched" to the local storage
+  // functioin of adding to "Watched" to the local storage by clicking "Add to Watched"
   function onWatchedClick() {
     if (!moviesWatched.find(item => item.id === data.id)) {
       moviesWatched.push(data);
       localStorage.setItem('movies-watched', JSON.stringify(moviesWatched));
 
-      if (addWatchedRef.textContent.includes('remove from watched')) {
-        addWatchedRef.classList.remove('js-remove-from');
-        addWatchedRef.textContent = 'add to Watched';
-        return;
-      }
+      const res = addWatchedRef.classList.toggle('js-remove-from');
+      addWatchedRef.textContent = `${res ? 'remove from' : 'add to'} watched `;
 
-      addWatchedRef.classList.toggle('js-remove-from');
-      addWatchedRef.textContent = 'remove from watched';
+      return;
     }
+
+    let index = moviesWatched.findIndex(object => object.id === data.id);
+
+    moviesWatched.splice(index, 1);
+    localStorage.setItem('movies-watched', JSON.stringify(moviesWatched));
+
+    const res = addWatchedRef.classList.toggle('js-remove-from');
+    addWatchedRef.textContent = `${res ? 'remove from' : 'add to'} watched `;
   }
 
   // functioin of adding to "Queue" to the local storage by clicking "Add to Queue"
