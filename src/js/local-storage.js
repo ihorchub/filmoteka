5910de90534d77ff0f468126d09035bcd0c26327
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-10 11:10:27 +0200

    Adds local-storage functions
    - option removing from local storage
    - adds functions for "Add to Queue"

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -2,21 +2,33 @@
 const moviesWatched = JSON.parse(localStorage.getItem('movies-watched')) || [];
 const moviesQueue = JSON.parse(localStorage.getItem('movies-queue')) || [];
 
+// function launches when the movie modal opens functioin "showModal()" from film-modal.js
 export function locStorage(data) {
   // reaching "Add to Watched" and "Add to Queue" buttons
   const addWatchedRef = document.querySelector('.add-watched');
   const addQueueRef = document.querySelector('.add-queue');
 
-  // adding listeners to "Add to Watched" and "Add to Queue" buttons by clicking to "Add to Watched"
+  // adding listeners to "Add to Watched" and "Add to Queue" buttons
   addWatchedRef.addEventListener('click', onWatchedClick);
   addQueueRef.addEventListener('click', onQueueClick);
 
+  // checker if the movie from the modal is already exists in local storage
+  // and make the buttons "Add to Watched" and "Add to Queue" appear accordingly
   if (localStorage.length > 0) {
     let savedWatchedArr = JSON.parse(localStorage.getItem('movies-watched'));
 
     if (savedWatchedArr.find(item => item.id === data.id)) {
       addWatchedRef.classList.add('js-remove-from');
       addWatchedRef.textContent = 'remove from watched';
+    }
+  }
+
+  if (localStorage.length > 0) {
+    let savedQueueArr = JSON.parse(localStorage.getItem('movies-queue'));
+
+    if (savedQueueArr.find(item => item.id === data.id)) {
+      addQueueRef.classList.add('js-remove-from');
+      addQueueRef.textContent = 'remove from queue';
     }
   }
 
@@ -46,6 +58,19 @@ export function locStorage(data) {
     if (!moviesQueue.find(item => item.id === data.id)) {
       moviesQueue.push(data);
       localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
+
+      const res = addQueueRef.classList.toggle('js-remove-from');
+      addQueueRef.textContent = `${res ? 'remove from' : 'add to'} queue `;
+
+      return;
     }
+
+    let index = moviesQueue.findIndex(object => object.id === data.id);
+
+    moviesQueue.splice(index, 1);
+    localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
+
+    const res = addQueueRef.classList.toggle('js-remove-from');
+    addQueueRef.textContent = `${res ? 'remove from' : 'add to'} queue `;
   }
 }
