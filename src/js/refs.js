26342cd90333b3821f1b6d80ccd2e6c0c3a8ff92
AlexLupeka <lupeka.alex@gmail.com>
2022-11-12 03:20:20 +0200

    modified js team-modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,6 @@ export const refs = {
   openTeamModal: document.querySelector('[data-team-modal-open]'),
   closeTeamModal: document.querySelector('[data-team-modal-close]'),
   teamModal: document.querySelector('[data-team-modal]'),
-  teamBackdrop: document.querySelector('.team-backdrop'),
+  teamBackdrop: document.querySelector('.team-modal-mask'),
   body: document.querySelector('body'),
 };
