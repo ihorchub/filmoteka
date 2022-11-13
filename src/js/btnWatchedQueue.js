0bdebd8ab648cdc232c2e890e4af47275bc146aa
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-11-13 12:58:43 +0200

    lib pagi

--- src/js/btnWatchedQueue.js
+++ src/js/btnWatchedQueue.js
@@ -1,5 +1,6 @@
 import { renderCards } from './renderCardsMyLibrary';
 import { spiner, spinerRemove, noInfo } from '../js/notifications';
+import { getPagination } from './pagination';
 
 // const watchedBtn = document.querySelector('.js-watched');
 // const queuedBtn = document.querySelector('.js-queue');
@@ -11,16 +12,20 @@ const QUEUE = 'movies-queue';
 // Заглушки порожньої сторінки
 const img = document.querySelector('.empty__page');
 const text = document.querySelector('.empty__page-text');
+let curPage = 1;
+const libPageSize = 18;
 
-export function onWatchedBtn() {
+export function onWatchedBtn(page = 0) {
+  console.log('w');
+  if (page) curPage = page;
+
   img.classList.add('is-hidden');
   text.classList.add('is-hidden');
 
   spiner();
   const obj = JSON.parse(localStorage.getItem(WATCHED));
   if (obj?.length) {
-    renderCards(obj);
-    spinerRemove();
+    calcLibSize(obj);
   } else {
     img.classList.remove('is-hidden');
     text.classList.remove('is-hidden');
@@ -29,19 +34,47 @@ export function onWatchedBtn() {
   // watchedBtn.removeEventListener('click', onWatchedBtn);
 }
 
-export function onQueuedBtn() {
+export function onQueuedBtn(page = 0) {
+  console.log('q');
+  if (page) curPage = page;
+
   img.classList.add('is-hidden');
   text.classList.add('is-hidden');
 
   spiner();
   const obj = JSON.parse(localStorage.getItem(QUEUE));
   if (obj?.length) {
-    renderCards(obj);
-    spinerRemove();
+    calcLibSize(obj);
   } else {
     img.classList.remove('is-hidden');
     text.classList.remove('is-hidden');
     spinerRemove();
   }
   // queuedBtn.removeEventListener('click', onQueuedBtn);
+}
+
+function calcLibSize(arr) {
+  let lastPage = null;
+
+  if (arr.length <= libPageSize) {
+    renderCards(arr);
+    spinerRemove();
+  }
+
+  // Визначення останньої сторінки
+  if (arr.length % libPageSize)
+    lastPage = Math.floor(arr.length / libPageSize) + 1;
+  else lastPage = arr.length / libPageSize;
+
+  // Рендер відповідної сторінки
+  if (curPage === lastPage) {
+    renderCards(arr.slice(curPage * libPageSize - libPageSize, arr.length));
+    spinerRemove();
+  } else {
+    renderCards(
+      arr.slice(curPage * libPageSize - libPageSize, curPage * libPageSize)
+    );
+    spinerRemove();
+  }
+  getPagination(curPage, lastPage, true);
 }
--- src/my-library.js
+++ src/my-library.js
@@ -7,6 +7,7 @@ import { onQueuedBtn, onWatchedBtn } from './js/btnWatchedQueue.js';
 import { spiner, spinerRemove, noInfo } from './js/notifications';
 import trailer from './js/film-trailer.js';
 import { showModal } from './js/film-modal';
+import { onSubmitScroll } from './js/onSubmit.js';
 
 export const refs = {
   cardHolderLibrary: document.querySelector('.card-holder'),
@@ -17,6 +18,7 @@ export const refs = {
   watchedBtn: document.querySelector('.js-watched'),
   queuedBtn: document.querySelector('.js-queue'),
   movieModal: document.querySelector('.js-movie-modal'),
+  pagination: document.querySelector('.pagination__container'),
 };
 
 const stickyMyLibrary = (window.onscroll = function showHeaderMyLibrary() {
@@ -34,6 +36,7 @@ refs.cardHolderLibrary.addEventListener('click', onCardClick);
 refs.watchedBtn.addEventListener('click', onWatched);
 refs.queuedBtn.addEventListener('click', onQueued);
 refs.movieModal.addEventListener('click', onModalClick);
+refs.pagination.addEventListener('click', onPagination);
 
 // Початкове завантаження
 onWatchedBtn();
@@ -44,7 +47,7 @@ function onWatched(e) {
   e.target.classList.add('my-library-header__button--current');
   refs.queuedBtn.classList.remove('my-library-header__button--current');
   refs.cardHolderLibrary.innerHTML = '';
-  onWatchedBtn();
+  onWatchedBtn(1);
 }
 
 function onQueued(e) {
@@ -53,7 +56,7 @@ function onQueued(e) {
   e.target.classList.add('my-library-header__button--current');
   refs.watchedBtn.classList.remove('my-library-header__button--current');
   refs.cardHolderLibrary.innerHTML = '';
-  onQueuedBtn();
+  onQueuedBtn(1);
 }
 
 function onCardClick(e) {
@@ -85,5 +88,45 @@ function onModalClick(e) {
   ) {
     refs.cardHolderLibrary.innerHTML = '';
     onQueuedBtn();
+  }
+}
+
+// Відстежування пагінації
+function onPagination(e) {
+  if (e.target === e.currentTarget || e.target.nodeName === 'UL') return;
+
+  let id = null;
+  if (
+    e.target.nodeName === 'svg' ||
+    e.target.nodeName === 'BUTTON' ||
+    e.target.nodeName === 'path'
+  ) {
+    if (
+      e.target.closest('button').classList.contains('pagination__left-btn') &&
+      e.target.closest('button').classList.contains('on')
+    )
+      id = Number(document.querySelector('.pagination__item--current').id) - 1;
+    else if (
+      e.target.closest('button').classList.contains('pagination__right-btn') &&
+      e.target.closest('button').classList.contains('on')
+    )
+      id = Number(document.querySelector('.pagination__item--current').id) + 1;
+    else return;
+  } else {
+    if (!isNaN(e.target.closest('li').id))
+      id = Number(e.target.closest('li').id);
+    else if (e.target.closest('li').id === '...')
+      return apiServise.query ? successPages(1, 1) : successPages(1);
+    else return;
+  }
+
+  if (
+    refs.watchedBtn.classList.contains('my-library-header__button--current')
+  ) {
+    onSubmitScroll();
+    onWatchedBtn(id);
+  } else {
+    onSubmitScroll();
+    onQueuedBtn(id);
   }
 }
